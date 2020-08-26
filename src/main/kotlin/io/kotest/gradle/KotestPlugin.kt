b1c2cec36646a568035880e92df96b1ff42c4a3c
sksamuel <sam@sksamuel.com>
2020-08-26 06:47:58 -0500

    Updated plugin to apply to subprojects

--- src/main/kotlin/io/kotest/gradle/KotestPlugin.kt
+++ src/main/kotlin/io/kotest/gradle/KotestPlugin.kt
@@ -5,9 +5,13 @@ import org.gradle.api.Project
 
 open class KotestPlugin : Plugin<Project> {
    override fun apply(project: Project) {
-      val task = project.tasks.create("kotest", KotestTask::class.java)
-      task.group = "verification"
-      task.description = "Runs Kotest tests"
-      task.dependsOn("classes", "testClasses")
+      project.allprojects.forEach {
+         if (it.tasks.none { task -> task.name == "kotest" }) {
+            val task = it.tasks.create("kotest", KotestTask::class.java)
+            task.group = "verification"
+            task.description = "Runs Kotest tests"
+            task.dependsOn("classes", "testClasses")
+         }
+      }
    }
 }
