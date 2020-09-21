1432f0d60cfb92715b109e428958fd2a7ba5bd14
sksamuel <sam@sksamuel.com>
2020-09-21 13:07:50 -0500

    Updated plugin to only depend on tasks that exist

--- src/main/kotlin/io/kotest/gradle/KotestPlugin.kt
+++ src/main/kotlin/io/kotest/gradle/KotestPlugin.kt
@@ -5,12 +5,15 @@ import org.gradle.api.Project
 
 open class KotestPlugin : Plugin<Project> {
    override fun apply(project: Project) {
-      project.allprojects.forEach {
-         if (it.tasks.none { task -> task.name == "kotest" }) {
-            val task = it.tasks.create("kotest", KotestTask::class.java)
+      project.subprojects.forEach { subproject ->
+         if (subproject.tasks.none { task -> task.name == "kotest" }) {
+            val task = subproject.tasks.create("kotest", KotestTask::class.java)
             task.group = "verification"
             task.description = "Runs Kotest tests"
-            task.dependsOn("classes", "testClasses")
+
+            val tasks = subproject.tasks.map { it.name }
+            val potentialDependsOn = arrayOf("classes", "testClasses").filter { tasks.contains(it) }.toTypedArray()
+            task.dependsOn(*potentialDependsOn)
          }
       }
    }
